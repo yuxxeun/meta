1e09ac7aaa857ce21f020de3a3853323f15a05ba
yuxxeun <itsyuxxeun@gmail.com>
2024-10-16 10:55:49 +0700

    fat(.config): lua conf

---
+++ .config/nvim/init.lua
@@ -0,0 +1,12 @@
+require("settings")
+require("plugins")
+require("maps") -- key mappings
+
+-- colorscheme config: kanagawa
+local themeStatus, kanagawa = pcall(require, "kanagawa")
+
+if themeStatus then
+    vim.cmd("colorscheme kanagawa")
+else
+    return
+end
