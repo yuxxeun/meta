ebfd7688e3a548cfd130530e029ca4f791ed02f6
yuxxeun <itsyuxxeun@gmail.com>
2025-06-06 17:19:32 +0700

    git: add aliases

---
+++ .config/git/aliases.sh
@@ -0,0 +1,13 @@
+alias ls='ls -F --color=auto --show-control-chars'
+alias ll='ls -l'
+alias g='git init'
+alias gc='git clone'
+alias ga='git add .'
+alias gcm='git commit -m'
+alias gpm='git push -u origin main'
+alias gr='git remote -v'
+alias gra='git remote add origin'
+
+#php composer
+alias pas='php artisan serve'
+alias pag='php artisan migrate'
