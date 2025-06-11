1fcd67544e28e7ae45d49b472a8666dd68f31398
yuxxeun <itsyuxxeun@gmail.com>
2025-06-12 06:10:57 +0700

    chore: update git-prompt

--- .config/git/git-prompt.sh
+++ .config/git/git-prompt.sh
@@ -4,18 +4,32 @@ then
 else
     TITLEPREFIX=$MSYSTEM
 fi
+
 if test -f ~/.config/git/git-prompt.sh
 then
     . ~/.config/git/git-prompt.sh
 else
-    PS1='\[\033]0;🔴🟡🟢\007\]'
-    PS1="$PS1"'\n'                 
-    PS1="$PS1"'\[\033[95m\]'       
-    PS1="$PS1"'\u '             	
-    PS1="$PS1"'\[\033[35m\]'       
-    PS1="$PS1"'🐱 '                
-    PS1="$PS1"'\[\033[33m\]'       
-    PS1="$PS1"'\w'                 
+    PS1='\[\033]0;🔴🟡🟢\007\]'    
+    PS1="$PS1"'\n'
+
+    PS1="$PS1"'\[\033[94m\]'
+    PS1="$PS1"'\u'                
+
+    PS1="$PS1"'\[\033[95m\]'
+    PS1="$PS1"' λ '
+
+    PS1="$PS1"'\[\033[94m\]'
+    PS1="$PS1"'\h '
+
+    PS1="$PS1"'\[\033[35m\]'
+    PS1="$PS1"'🐰 '
+
+    PS1="$PS1"'\[\033[92m\]'
+    PS1="$PS1"'⬢ v22.16.0 '
+
+    PS1="$PS1"'\[\033[33m\]'
+    PS1="$PS1"'\w'
+
     if test -z "$WINELOADERNOEXEC"
     then
         GIT_EXEC_PATH="$(git --exec-path 2>/dev/null)"
@@ -26,15 +40,18 @@ else
         then
             . "$COMPLETION_PATH/git-completion.bash"
             . "$COMPLETION_PATH/git-prompt.sh"
-            PS1="$PS1"'\[\033[36m\]'  
-            PS1="$PS1"'`__git_ps1`'   
+
+            PS1="$PS1"'\[\033[36m\]'
+            PS1="$PS1"'`__git_ps1`'
         fi
     fi
-    PS1="$PS1"'\[\033[0m\]'        
-    PS1="$PS1"'\n'                 
-    PS1="$PS1"'$ '                 
+
+    PS1="$PS1"'\[\033[0m\]'
+    PS1="$PS1"'\n'
+    PS1="$PS1"'$ '
 fi
-MSYS2_PS1="$PS1"               
+
+MSYS2_PS1="$PS1"
 
 if test -z "$WINELOADERNOEXEC"
 then
